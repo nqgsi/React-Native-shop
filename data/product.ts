d6215698dd021c9816b8a7be3bc84afaf3aecc8a
nawaf <nawafalrenezi@gmail.com>
2025-07-28 17:09:06 +0300

    bith tasks finished

---
+++ data/product.ts
@@ -0,0 +1,18 @@
+interface c {
+  pname: string;
+  price: number;
+  image: string;
+}
+
+const cookies: c = {
+  pname: "Chocolate Chip",
+  price: 2.99,
+  image:
+    " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6XxzWY7xzhMmyWCzDCmtmCjTjWR-tvJoxQ&s",
+};
+const rcookies: c = {
+  pname: "rainbowCookies",
+  price: 4.99,
+  image:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6XxzWY7xzhMmyWCzDCmtmCjTjWR-tvJoxQ&s",
+};
